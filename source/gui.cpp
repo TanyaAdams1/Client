9d56a72378c61ea5d108902649a05a4877d7a529
yinke6 <757664343@qq.com>
2016-12-29 13:13:45 +0800

    Update gui.cpp

--- source/gui.cpp
+++ source/gui.cpp
@@ -1,29 +1,113 @@
 #include "gui.h"
-#include<gamewindow.h>
-gui::gui()
+#include "createroom.h"
+#include "hall.h"
+#include "warning.h"
+#include "login.h"
+#include <QtCore>
+#include <QObject>
+#include <QDesktopServices>
+#include <QUrl>
+
+
+
+gui::gui(QObject *parent)
+    :QObject(parent)
 {
+    QObject::connect(&w,&hall::createbutton_clicked,this,&gui::showcreateroom);
+    QObject::connect(&c,&createroom::newroom,this,&gui::onenewroom);
+    QObject::connect(&w,&hall::enterroom,this,&gui::enterroom);
+    QObject::connect(&w,&hall::openlogin,this,&gui::openlogin);
+    QObject::connect(&w,&hall::openweb,this,&gui::openweb);
+    QObject::connect(&w,&hall::warning,this,&gui::WArning);
+
     QObject::connect(&g,&GameWindow::prepared,this,&gui::gprepared);
     QObject::connect(&g,&GameWindow::unprepared,this,&gui::gunprepared);
     QObject::connect(&g,&GameWindow::goback,this,&gui::ggoback);
     QObject::connect(&g,&GameWindow::endturn,this,&gui::gendturn);
     QObject::connect(&g,&GameWindow::explode,this,&gui::gexplode);
 }
-void gui::back(bool permission)
-{
-    g.back(permission);
+
+QPair<QString,int> gui::acquireServer(){
+    l.exec();
+    return l.getpair();
 }
+
+void gui::showHall(){
+    w.setup(l.getpair().first,QString::number(l.getpair().second,10));
+    w.show();
+}
+
+void gui::showlogin(){
+    l.exec();
+}
+
+void gui::showcreateroom(){
+    c.exec();
+}
+
+void gui::onenewroom(const int number){
+    emit Newroom(number);
+    c.hide();
+}
+
+void gui::warning(){
+    Warning warning;
+    warning.exec();
+}
+
+void gui::flushroom(QVector<QVector<int> > vect){
+    removeroom();
+    int i=vect.size();
+    for(int j=0;j<i;j++){
+        QString a=QString::number(vect[j][0],10);
+        QString b=QString::number(vect[j][2],10)+"/"+QString::number(vect[j][1],10)+"人";
+        w.addroom(a,b);
+    }
+}
+
+void gui::removeroom(){
+    w.removeall();
+}
+
+void gui::enterroom(const int id){
+    emit enterRoom(id);
+}
+
+void gui::enterroomok(){
+    w.hide();
+    g.show();
+}
+
+void gui::openlogin(){
+    w.hide();
+    l.reset();
+    l.show();
+}
+
+void gui::openweb(){
+    QDesktopServices::openUrl(QUrl("http://baike.baidu.com/link?url=NIp1tudVwrioy0g3GF4QEles_ctV6TEpD_-sQ21huAlzyu7m-g4l1WrnQzhSovRjZpaJ1Zf3hMsrjejzpOESFludN9CZfCfA2F4PPDvXEdYTww_NCJP6unzDD6DiNGGd"));
+}
+
+void gui::hidehall(){
+    w.hide();
+}
+
+void gui::WArning(){
+    warning();
+}
+
 
 void gui::showgame()
 {
     g.show();
 }
 
-void gui::flush(QVector<QPair<int, int> > vect, int prepared, int sum)
+void gui::flush(QVector<QPair<int, int> > vect, int prepared)
 {
     QVector<QPair<int,int>>::iterator i;
     for(i=vect.begin();i!=vect.end();i++){
     g.addplayer(i->first,i->second);}
-    g.showprepared(prepared,sum);
+    g.showprepared(prepared);
 }
 void gui::myplayer(int seat, int id)
 {
